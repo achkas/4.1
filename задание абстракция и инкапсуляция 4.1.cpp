80c2840b6f03694b8be58fdaa45a54e573b24cc1
Igor Achkas <jigoren@yandex.ru>
2023-05-30 21:25:16 +0700

    add 1

---
+++ задание абстракция и инкапсуляция 4.1.cpp
@@ -0,0 +1,175 @@
+﻿// задание абстракция и инкапсуляция 4.1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string>
+#include <windows.h>
+#include <fstream>
+
+class location {
+    
+    
+      
+    std::string city ;
+    std::string street;
+    std::string house ;
+    std::string apartment;
+    
+public:
+    ///*const int size;*/
+    //location(std::string city, std::string street, std::string house, std::string apartment, 
+    //    /*int size, location* arr,*//*const int size, location in*/ )
+    //{
+    //    location* arr[sizeof(arr) / sizeof(arr[0])];
+
+    //    //location arr[sizeof(arr) / sizeof(arr[1])];
+    //    for (int i = 0; i < size; ++i) {
+    //        arr[i].city;
+    //        arr[i].street;
+    //        arr[i].house;
+    //        arr[i].apartment;
+    //    }
+    //       
+    //}
+
+        location(std::string city, std::string street, std::string house, std::string apartment)
+    {
+        
+        this->city = city;
+        this->street = street;
+        this->house = house;
+        this->apartment = apartment;
+
+    }
+
+    void set_inc(std::string city)
+    {
+        this->city = city;
+    }
+
+    void set_ins(std::string street)
+    {
+        this->street = street;
+    }
+
+    void set_inh(std::string house)
+    {
+        this->house = house;
+    }
+
+    void set_ina(std::string apartment)
+    {
+        this->apartment = apartment;
+    }
+
+    /*std::string get_in() {
+        return  city;
+        
+    }
+
+    std::string get_ins() {
+        
+        return  street;
+    }*/
+    
+
+    void setout(location* arr, int size, location in2) {
+        std::ofstream rf;
+        rf.open("out.txt");
+        //std::cout << rf .is_open() << std::endl; 
+
+        rf << size;
+        rf << "\n";
+        
+            for (int i = 0; i < size / 2; i++) {
+               
+                
+                in2 = arr[i];
+                arr[i] = arr[(size - i) - 1];
+                arr[(size - i) - 1] = in2;
+            }
+
+            for (int i = 0; i < size; i++){
+
+                rf << arr[i].city + ",  " + arr[i].street + ",  " 
+                    + arr[i].house + ",  " + arr[i].apartment + "\n";
+            
+        }               
+
+        rf.close();
+    }
+
+   
+    
+  location() = default;
+   
+    
+    void print_array(location* arr, int size) {
+        std::cout << size << std::endl;
+        for (int i = 0; i < size; ++i) {
+            std::cout                
+                << arr[i].city << std::endl
+                << arr[i].street << std::endl
+                << arr[i].house << std::endl
+                << arr[i].apartment << std::endl;
+            std::cout << std::endl;
+        }
+
+    }   
+
+    
+
+   
+
+    
+};
+
+
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    location in(" ", " ", " ", " ");
+    location in1(" ", " ", " ", " ");
+    location in2(" ", " ", " ", " ");   
+
+    int size;
+    std::string city;
+    std::string street;
+    std::string house;
+    std::string apartment;
+
+    std::ifstream wf1;
+    wf1.open("in.txt");
+    // std::cout << wf1 .is_open() << std::endl;
+
+    wf1 >> size; 
+      
+     location* loc_in_array = new location[size];
+      for (int i = 0; i < size; ++i) {
+       wf1 >> city;
+       loc_in_array [i].set_inc(city);
+       wf1 >> street;
+       loc_in_array[i].set_ins(street);
+       wf1 >> house;
+       loc_in_array[i].set_inh(house);
+       wf1 >> apartment;
+       loc_in_array[i].set_ina(apartment);
+     
+ }
+
+      wf1.close();
+
+ in.print_array(loc_in_array, size);
+
+ in.setout(loc_in_array, size, in2);   
+
+    delete[] loc_in_array;
+
+    return 0;
+
+    
+}
